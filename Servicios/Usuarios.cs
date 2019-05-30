fd42fd43cccf77ac27bbcd419ef401350c3f5cae
Cristian Martin Sguazzin <34490158+NeXuZZ-SCM@users.noreply.github.com>
2019-05-30 17:03:33 -0300

    Add files via upload

---
+++ Servicios/Usuarios.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class Usuarios
+    {
+        #region ABM
+
+        #region Alta
+        public static int AgregarUsuario(Entidades.Usuario Usuario)
+        {
+            return Datos.UsuariosCRUD.AgregarUsuario(Usuario);
+        }
+        #endregion
+
+        #region BAJA
+        public static void EliminarUsuario(Entidades.Usuario usuario)
+        {
+            Datos.UsuariosCRUD.EliminarUsuario(usuario);
+        }
+        #endregion
+
+        #region Modificacion
+        public static void EditarUsuarios(Entidades.Usuario usuario)
+        {
+            Datos.UsuariosCRUD.EditarUsuarios(usuario);
+        }
+        #endregion
+
+
+        #endregion
+
+        #region Show Table
+        public static DataTable obtenerTabla()
+        {
+            return Datos.UsuariosCRUD.ObtenerTabla();
+        }
+        #endregion
+
+    }
+}
